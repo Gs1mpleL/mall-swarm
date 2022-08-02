b2cd3c17a57a02b601980ae90fa9b8c5266f863b
liuzhuohao <804121985@qq.com>
2022-08-02 15:40:50 +0800

    修复：方法注解修复

--- mall-portal/src/main/java/com/macro/mall/portal/controller/OmsCartItemController.java
+++ mall-portal/src/main/java/com/macro/mall/portal/controller/OmsCartItemController.java
@@ -30,7 +30,7 @@ public class OmsCartItemController {
     @ApiOperation("添加商品到购物车")
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
-    public CommonResult add(@RequestBody OmsCartItem cartItem) {
+    public CommonResult add(OmsCartItem cartItem) {
         int count = cartItemService.add(cartItem);
         if (count > 0) {
             return CommonResult.success(count);
